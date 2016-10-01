02ad46f9a16b4522ccb60ce2636595bd4e641bdc
Eric Hebert <ayebear@gmail.com>
2016-10-01 14:33:08 -0400

    Updated server to handle multiple members

--- index.js
+++ index.js
@@ -2,17 +2,35 @@ var io = require('socket.io')();
 
 let clients = {};
 
+/*
+{
+	socketId: {
+		username: "fdfdgdfg",
+		instrument: "drums"
+	}
+}
+*/
+
 io.on('connection', function(socket) {
 	// Add client
-	socket.emit('test', {message: `Hello client ${socket.id}!!!`});
+	// socket.emit('test', {message: `Hello client ${socket.id}!!!`});
 
-	socket.on('event', function(data) {
-		data.id = socket.id
+	socket.on('update_member', function(data) {
+		clients[socket.id] = data;
+		socket.emit('members_updated', clients);
+		console.log('Client ${socket.id} changed:');
 		console.log(data);
+	});
+
+	socket.on('play', function(data) {
+		data.id = socket.id;
+		io.emit('play', data);
 	});
 
 	socket.on('disconnect', function() {
 		// Remove client
+		delete clients[socket.id];
+		socket.emit('members_updated', clients);
 		console.log(`Client ${socket.id} disconnected.`);
 	});
 });
